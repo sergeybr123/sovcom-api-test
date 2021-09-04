52adf12ec60d7e7ebd2f0dc8753b66bc0c3b9b4c
Sergey <sergey.bryntsov@gmail.com>
2021-09-05 05:56:22 +0600

    init

---
+++ database/migrations/2021_09_04_230557_create_currencies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurrenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->id();
+            $table->string('currency', 3)->unique();
+            $table->float('bay', 5, 2)->default(0.00);
+            $table->float('sell', 5, 2)->default(0.00);
+            $table->dateTime('begin_at')->nullable();
+            $table->integer('office_id')->unsigned()->nullable();
+            $table->timestamps();
+            $table->softDeletes('delete_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+}
